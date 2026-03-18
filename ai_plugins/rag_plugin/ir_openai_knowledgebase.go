3c7065827adf75002dcba1336c39c0d5f2589ca3
abdomash <abdomash2010@gmail.com>
2026-03-18 15:22:00 +0300

    ai_plugins: add knowledgebase and vectorstore integrations

---
+++ ai_plugins/rag_plugin/ir_openai_knowledgebase.go
@@ -0,0 +1,78 @@
+package rag_plugin
+
+import (
+	"fmt"
+	"log/slog"
+
+	"github.com/blueprint-uservices/blueprint/blueprint/pkg/coreplugins/service"
+	"github.com/blueprint-uservices/blueprint/blueprint/pkg/ir"
+	"github.com/blueprint-uservices/blueprint/plugins/golang"
+	"github.com/blueprint-uservices/blueprint/plugins/workflow/workflowspec"
+	ragruntime "github.com/vaastav/agentic_blueprint/ai_runtime/plugins/rag"
+)
+
+type OpenAIKnowledgeBaseClient struct {
+	golang.Service
+	ir.IRNode
+	service.ServiceNode
+
+	Spec           *workflowspec.Service
+	ClientName     string
+	OpenAIURL      string
+	APIKey         string
+	EmbeddingModel string
+	VectorStore    ir.IRNode
+}
+
+func newOpenAIKnowledgeBaseClient(name string, openaiURL string, apiKey string, embeddingModel string, vectorStore ir.IRNode) (*OpenAIKnowledgeBaseClient, error) {
+	spec, err := workflowspec.GetService[ragruntime.OpenAIKnowledgeBase]()
+	if err != nil {
+		return nil, err
+	}
+	return &OpenAIKnowledgeBaseClient{
+		Spec:           spec,
+		ClientName:     name,
+		OpenAIURL:      openaiURL,
+		APIKey:         apiKey,
+		EmbeddingModel: embeddingModel,
+		VectorStore:    vectorStore,
+	}, nil
+}
+
+func (node *OpenAIKnowledgeBaseClient) Name() string {
+	return node.ClientName
+}
+
+func (node *OpenAIKnowledgeBaseClient) String() string {
+	return node.Name() + " = OpenAIKnowledgeBase(" + node.VectorStore.Name() + ")"
+}
+
+func (node *OpenAIKnowledgeBaseClient) AddInstantiation(builder golang.NamespaceBuilder) error {
+	if builder.Visited(node.ClientName) {
+		return nil
+	}
+
+	slog.Info(fmt.Sprintf("Instantiating OpenAIKnowledgeBaseClient %v in %v/%v", node.ClientName, builder.Info().Package.PackageName, builder.Info().FileName))
+
+	constructor := node.Spec.Constructor.AsConstructor()
+	return builder.DeclareConstructor(node.ClientName, constructor, []ir.IRNode{
+		&ir.IRValue{Value: node.OpenAIURL},
+		&ir.IRValue{Value: node.APIKey},
+		&ir.IRValue{Value: node.EmbeddingModel},
+		node.VectorStore,
+	})
+}
+
+func (node *OpenAIKnowledgeBaseClient) AddToWorkspace(builder golang.WorkspaceBuilder) error {
+	return node.Spec.AddToWorkspace(builder)
+}
+
+func (node *OpenAIKnowledgeBaseClient) AddInterfaces(builder golang.ModuleBuilder) error {
+	return node.Spec.AddToModule(builder)
+}
+
+func (node *OpenAIKnowledgeBaseClient) GetInterface(ctx ir.BuildContext) (service.ServiceInterface, error) {
+	return node.Spec.Iface.ServiceInterface(ctx), nil
+}
+
+func (node *OpenAIKnowledgeBaseClient) ImplementsGolangNode() {}
---
+++ ai_plugins/rag_plugin/wiring.go
@@ -0,0 +1,75 @@
+package rag_plugin
+
+import (
+	"github.com/blueprint-uservices/blueprint/blueprint/pkg/coreplugins/pointer"
+	"github.com/blueprint-uservices/blueprint/blueprint/pkg/ir"
+	"github.com/blueprint-uservices/blueprint/blueprint/pkg/wiring"
+	"github.com/vaastav/agentic_blueprint/ai_runtime/core"
+	ragruntime "github.com/vaastav/agentic_blueprint/ai_runtime/plugins/rag"
+)
+
+type ToolExposure = ragruntime.ToolExposure
+
+const (
+	NoTools    = ragruntime.NoTools
+	SearchOnly = ragruntime.SearchOnly
+	FullCRUD   = ragruntime.FullCRUD
+)
+
+type RAGAgentConfig = ragruntime.RAGAgentConfig
+
+func KnowledgeBase[Impl core.KnowledgeBase](spec wiring.WiringSpec, name string) string {
+	backendName := name + ".knowledge_base"
+
+	spec.Define(backendName, &KnowledgeBaseClient{}, func(ns wiring.Namespace) (ir.IRNode, error) {
+		return newKnowledgeBaseClient[Impl](name)
+	})
+
+	pointer.CreatePointer[*KnowledgeBaseClient](spec, name, backendName)
+	return name
+}
+
+func VectorStore[Impl core.VectorStore](spec wiring.WiringSpec, name string) string {
+	backendName := name + ".vector_store"
+
+	spec.Define(backendName, &VectorStoreClient{}, func(ns wiring.Namespace) (ir.IRNode, error) {
+		return newVectorStoreClient[Impl](name)
+	})
+
+	pointer.CreatePointer[*VectorStoreClient](spec, name, backendName)
+	return name
+}
+
+func OpenAIKnowledgeBase(spec wiring.WiringSpec, name string, openaiURL string, apiKey string, embeddingModel string, vectorStoreName string) string {
+	backendName := name + ".openai_knowledge_base"
+
+	spec.Define(backendName, &OpenAIKnowledgeBaseClient{}, func(ns wiring.Namespace) (ir.IRNode, error) {
+		var vectorStore ir.IRNode
+		if err := ns.Get(vectorStoreName, &vectorStore); err != nil {
+			return nil, err
+		}
+		return newOpenAIKnowledgeBaseClient(name, openaiURL, apiKey, embeddingModel, vectorStore)
+	})
+
+	pointer.CreatePointer[*OpenAIKnowledgeBaseClient](spec, name, backendName)
+	return name
+}
+
+func RAGAgent(spec wiring.WiringSpec, name string, baseAgent string, kb string, config RAGAgentConfig) string {
+	backendName := name + ".rag_agent"
+
+	spec.Define(backendName, &RAGAgentClient{}, func(ns wiring.Namespace) (ir.IRNode, error) {
+		var innerAgent ir.IRNode
+		if err := ns.Get(baseAgent, &innerAgent); err != nil {
+			return nil, err
+		}
+		var knowledgeBase ir.IRNode
+		if err := ns.Get(kb, &knowledgeBase); err != nil {
+			return nil, err
+		}
+		return newRAGAgentClient(name, innerAgent, knowledgeBase, config.ToolExposure, config.AutoQuery, config.TopK, config.AutoIndex)
+	})
+
+	pointer.CreatePointer[*RAGAgentClient](spec, name, backendName)
+	return name
+}
