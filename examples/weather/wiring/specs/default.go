d8563cf1d345464a37ab62801743fc0094aed572
Vaastav Anand <vaastav.anand05@gmail.com>
2026-04-23 12:49:23 +0200

    Update use of model

--- examples/weather/wiring/specs/default.go
+++ examples/weather/wiring/specs/default.go
@@ -1,11 +1,6 @@
 package specs
 
 import (
-	"encoding/json"
-	"flag"
-	"io/ioutil"
-	"os"
-
 	"github.com/blueprint-uservices/blueprint/blueprint/pkg/wiring"
 	"github.com/blueprint-uservices/blueprint/plugins/cmdbuilder"
 	"github.com/blueprint-uservices/blueprint/plugins/goproc"
@@ -13,23 +8,16 @@ import (
 	"github.com/blueprint-uservices/blueprint/plugins/linuxcontainer"
 	"github.com/blueprint-uservices/blueprint/plugins/workflow"
 
+	"github.com/vaastav/agentic_blueprint/ai_plugins/model"
 	"github.com/vaastav/agentic_blueprint/ai_plugins/openai_plugin"
 	wf "github.com/vaastav/agentic_blueprint/examples/weather/workflow"
 )
-
-type ModelInfo struct {
-	Name string `json:"name"`
-	URL  string `json:"url"`
-	Key  string `json:"key"`
-}
 
 var Docker = cmdbuilder.SpecOption{
 	Name:        "docker",
 	Description: "Deploys each agent in a separate container with http connecting the agents, uses OpenAI for providing the agents",
 	Build:       makeDockerSpec,
 }
-
-var model_file = flag.String("modfile", "model.json", "Specific model related information")
 
 func makeDockerSpec(spec wiring.WiringSpec) ([]string, error) {
 
@@ -39,18 +27,7 @@ func makeDockerSpec(spec wiring.WiringSpec) ([]string, error) {
 		return linuxcontainer.Deploy(spec, serviceName)
 	}
 
-	var minfo ModelInfo
-	file, err := os.Open(*model_file)
-	if err != nil {
-		return []string{}, err
-	}
-	defer file.Close()
-
-	all_bytes, err := ioutil.ReadAll(file)
-	if err != nil {
-		return []string{}, err
-	}
-	err = json.Unmarshal(all_bytes, &minfo)
+	minfo, err := model.GetModelInfo()
 	if err != nil {
 		return []string{}, err
 	}
